8662585e41e22088e468b2b9babdf743d5d02dc2
Thiago Novato <thiagonovato@gmail.com>
2023-01-13 07:13:43 -0500

    first commit

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,22 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    this.server.emit('message', message);
+  }
+}
